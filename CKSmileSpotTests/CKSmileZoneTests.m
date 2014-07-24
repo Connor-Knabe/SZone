a3ab6c9a55c8d573c38ed0f32a2504f8a717996d
Administrator <connorknabe@gmail.com>
2014-07-23 19:07:09 -0500

    Setup a test class to test the score view controller

--- CKSmileSpotTests/CKSmileZoneTests.m
+++ CKSmileSpotTests/CKSmileZoneTests.m
@@ -36,9 +36,6 @@
 
 -(void)testThatUsernameCanBeAddedToArray{
     XCTAssertNotNil(self.userArray);
-
-    
-    
 }
 
 
---
+++ CKSmileSpotTests/CKTestScoreViewController.m
@@ -0,0 +1,23 @@
+//
+//  CKTestScoreViewController.m
+//  CKSmileZone
+//
+//  Created by Administrator on 7/23/14.
+//  Copyright (c) 2014 Connor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CKTestScoreViewController : XCTestCase
+
+@end
+
+@implementation CKTestScoreViewController
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+
+@end
