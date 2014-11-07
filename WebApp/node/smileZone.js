23d0f0ea6acec4697ed6fa2d672915644b23f1cb
ObiwanBox <connorknabe@gmail.com>
2014-11-07 11:37:59 -0500

    Alerts user of their geo location

--- WebApp/node/smileZone.js
+++ WebApp/node/smileZone.js
@@ -1,4 +1,4 @@
-var express = require('express");
+var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var app = express();
@@ -6,13 +6,15 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.configure(function(){
-
-		app.set('views',__dirname + '/views');
-		app.set('view engine', 'ejs');
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 	res.render('index');
 });
 
+app.post('/submit', function(req, res){
+	res.render('geo');
+
+});
+app.listen(80);
