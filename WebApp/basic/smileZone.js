2fddf38c67dfece0b3c194cbc4b3efd50673b380
ObiwanBox <connorknabe@gmail.com>
2014-11-07 11:24:30 -0500

    Added template

--- WebApp/basic/smileZone.js
+++ WebApp/basic/smileZone.js
@@ -3,3 +3,16 @@ var bodyParser = require('body-parser');
 var http = require('http');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.configure(function(){
+
+		app.set('views',__dirname + '/views');
+		app.set('view engine', 'ejs');
+});
+
+app.get('/', function(req,res){
+	res.render('index');
+});
+
