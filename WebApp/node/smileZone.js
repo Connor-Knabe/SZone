c6770c3a2d48da8c06af5730af37cddafe8dab69
ObiwanBox <connorknabe@gmail.com>
2014-11-08 12:33:21 -0500

    Using ipinfo.io

--- WebApp/node/smileZone.js
+++ WebApp/node/smileZone.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var app = express();
+var url = require('url');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -13,16 +14,43 @@ app.get('/', function(req,res){
 	res.render('index');
 });
 
+
+
 app.post('/submit', function(req, res){
 	var latitude = req.body.lat;
 
 	var longitude = req.body.longitude;
 	var name = req.body.name;
+	var ip = req.connection.remoteAddress;
+
 	console.log(req.body);
-	console.log(latitude);
-	console.log(longitude);
+	console.log(ip);
+
+
+
+
+
+	var options = {
+	  host: 'ipinfo.io',
+	  port: 80,
+	  path: '/'+ip+'/json',
+	  method: 'POST'
+	};
+
+	var ipinfoUrl = url.format(options);
+	request(ipinfoUrl, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			//Sends the parsed data from rotten tomatoes to ejs template
+			var parsedData = JSON.parse(body);
+			console.log(parsedData);
+		}
+
+	});
+
+
 
 	res.render('index');
-
 });
+
+
 app.listen(80);
