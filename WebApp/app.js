69134f9b999efff7d2b0e385a5148a45ecb9ee62
ObiwanBox <connorknabe@gmail.com>
2014-11-10 11:11:15 -0500

    Added include to app.js

---
+++ WebApp/app.js
@@ -0,0 +1,10 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 80;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path = require('path');
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer(app);
