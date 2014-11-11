d8b9c2ab3c8c8e8210bfc8aa80cbee14a89b7505
ObiwanBox <connorknabe@gmail.com>
2014-11-11 14:27:53 -0500

    Changed directory to load index.ejs from views folder

--- WebApp/controller/routes.js
+++ WebApp/controller/routes.js
@@ -2,6 +2,6 @@ var User = require('../models/user');
 
 module.exports = function(app,passport,server) {
 	app.get('/', function(req, res) {
-		res.render('template/index.ejs');
+		res.render('index.ejs');
 	});
 }
