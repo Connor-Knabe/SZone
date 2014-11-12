6a3d9f6ec9e3089e7ee781e40fe61f56fa87bb70
ObiwanBox <connorknabe@gmail.com>
2014-11-12 11:25:23 -0500

    Signup form being generated using templates

--- WebApp/config/routes.js
+++ WebApp/config/routes.js
@@ -2,7 +2,7 @@ var User = require('../models/user');
 
 module.exports = function(app,passport,server) {
 	app.get('/', function(req, res) {
-		res.render('index.ejs');
+		res.render('index.ejs',{action:"index"});
 		console.log("index");
 	});
 	app.get('/signup', function(req, res) {
