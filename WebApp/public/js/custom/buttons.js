87fa305d231ca2de6a207245db71b011c5a89d8c
ObiwanBox <connorknabe@gmail.com>
2014-12-03 10:58:53 -0500

    Sending info to last10 post

--- WebApp/public/js/custom/buttons.js
+++ WebApp/public/js/custom/buttons.js
@@ -1,59 +1,67 @@
 function pt1 (){
     totalPoints += 1;
-    $( "#points" ).html(totalPoints);
-    form_send(1,$("#notebox_area").val(),latitude,longitude);
+    $( '#points' ).html(totalPoints);
+    form_send(1,$('#notebox_area').val(),latitude,longitude);
     add_marker(latitude,longitude);
 }
-
 function pt2 (){
     totalPoints += 2;
-    $( "#points" ).html(totalPoints);
-    form_send(2,$("#notebox_area").val(),latitude,longitude);
+    $( '#points' ).html(totalPoints);
+    form_send(2,$('#notebox_area').val(),latitude,longitude);
     add_marker(latitude,longitude);
 }
-
 function pt3 (){
     totalPoints += 3;
-    $( "#points" ).html(totalPoints);
-    form_send(3,$("#notebox_area").val(),latitude,longitude);
+    $( '#points' ).html(totalPoints);
+    form_send(3,$('#notebox_area').val(),latitude,longitude);
     add_marker(latitude,longitude);
 }
-
 function pt5 (){
     totalPoints += 5;
-    $( "#points" ).html(totalPoints);
-    form_send(5, $("#notebox_area").val(),latitude,longitude);
+    $( '#points' ).html(totalPoints);
+    form_send(5, $('#notebox_area').val(),latitude,longitude);
     add_marker(latitude,longitude);
 }
-
-
 function note (){
-	if ($("#notebox").css("display") == "none") {
-		$( "#notebox" ).show();
+	if ($('#notebox').css('display') == 'none') {
+		$( '#notebox' ).show();
     } else {
-		$( "#notebox" ).hide();
+		$( '#notebox' ).hide();
     }
 }
-
 function notebox_click (){
-	$("#notebox_area").val('');
+	$('#notebox_area').val('');
 }
 
+function last10 (){
+	alert("last10 clicked");//for debugging
+	
+	form_last10();
+}
+
+
+
 function load_binds(){
-    $( "#pt1" ).unbind( "click", pt1 );
+
+	//Bind the click event to a method
+    $( '#pt1' ).unbind( 'click', pt1 );
     $('#pt1').bind('click', pt1);
-    $( "#pt2" ).unbind( "click", pt2 );
+    $( '#pt2' ).unbind( 'click', pt2 );
     $('#pt2').bind('click', pt2);
-    $( "#pt3" ).unbind( "click", pt3 );
+    $( '#pt3' ).unbind( 'click', pt3 );
     $('#pt3').bind('click', pt3);
-    $( "#pt5" ).unbind( "click", pt5 );
+    $( '#pt5' ).unbind( 'click', pt5 );
     $('#pt5').bind('click', pt5);
-    $( "#note" ).unbind( "click", note );
+    $( '#note' ).unbind( 'click', note );
     $('#note').bind('click', note);
-
-	$( "#notebox_area" ).unbind( "click", notebox_click );
+	$( '#notebox_area' ).unbind( 'click', notebox_click);
     $('#notebox_area').bind('click', notebox_click);
+    $( '#last10' ).unbind( 'click', last10);
+    $('#last10').bind('click', last10);
 
+
+
+	//For mobile devices since the highlighting would not go away after click event
     $('#pt1').bind('touchstart', function(){
         $(this).addClass('btn2');
     }).bind('touchend', function(){
@@ -79,5 +87,9 @@ function load_binds(){
     }).bind('touchend', function(){
         $(this).removeClass('btn2');
     });
+    $('#last10').bind('touchstart', function(){
+        $(this).addClass('btn2');
+    }).bind('touchend', function(){
+        $(this).removeClass('btn2');
+    });
 }
-
