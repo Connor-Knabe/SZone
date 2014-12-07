c1775efdd289827650a7ba1d093bb987fa006f11
ObiwanBox <connorknabe@gmail.com>
2014-12-07 12:45:28 -0500

    Changed the model

--- WebApp/mvc/models/point.js
+++ WebApp/mvc/models/point.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 exports.pointsSchema = mongoose.Schema({
 	email: { type: String, required: true, unique: false },
-    points: [mongoose.Schema.Types.Mixed],
+	date: { type: String, required: false, unique: false },
+	pointAmt:{ type: Number, required: false, unique: false },
+    gps: [mongoose.Schema.Types.Mixed],
     notes: { type: String, required: false, unique: false }
 });
