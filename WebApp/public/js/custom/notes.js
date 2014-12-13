970c6a98d4efdb3a1704dabc03a60d689eddb577
ObiwanBox <connorknabe@gmail.com>
2014-12-13 17:30:40 -0500

    Removed jquery from .done

--- WebApp/public/js/custom/notes.js
+++ WebApp/public/js/custom/notes.js
@@ -4,6 +4,6 @@ function load_notes(){
         url: "/loadNotes",
 	})
 	.done(function( data ) {
-		$( "#ip" ).append( '<p id="red"> You have not allowed GPS tracking, using your IP address instead(less accurate). </p>');				
+
     });
 }
