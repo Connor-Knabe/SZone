ff95df8a31cb4b08a56d1ee57a209c27ddaf2c95
ObiwanBox <connorknabe@gmail.com>
2014-12-14 18:01:00 -0500

    Adding notes to the page when page loads

--- WebApp/public/js/custom/notes.js
+++ WebApp/public/js/custom/notes.js
@@ -4,6 +4,14 @@ function load_notes(){
         url: "/loadNotes",
 	})
 	.done(function( data ) {
-
+        var resultsArray = data.queryResults;
+		for(var i=0;i<resultsArray.length;i++){
+			if(resultsArray[i].notes == ""){
+                //add note that is blank			    
+			} else {
+			    //add note to page with info
+			}
+			
+		}
     });
 }
