72582098de634e90459e1c7d9402b4675fdb3548
ObiwanBox <connorknabe@gmail.com>
2015-02-22 12:38:22 -0500

    Added favicon

--- WebApp/app.js
+++ WebApp/app.js
@@ -52,6 +52,7 @@ app.configure(function() {
     app.use(express.urlencoded());
  	app.use(express.cookieParser());
 	app.use(express.static(__dirname + '/public'));
+	app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 	app.set('views', __dirname + '/mvc/views');
     app.set('view engine', 'ejs');
 	app.use(express.session({ secret: sessionSecret.secret }));
